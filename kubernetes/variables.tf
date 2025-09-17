d2aa7651b48f6b285ad56fc71599a57f48ade329
srteixeiradias <samuelteixeiradias008@gmail.com>
2025-09-16 23:20:27 -0300

    feat(kubernetes) : add variables for db rds

---
+++ kubernetes/variables.tf
@@ -0,0 +1,16 @@
+variable "db_username" {
+  description = "Master username for the database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_endpoint" {
+  description = "RDS endpoint"
+  type        = string
+  default     = "fastfood-soat-db.clm8cegucd6a.sa-east-1.rds.amazonaws.com"
+}
