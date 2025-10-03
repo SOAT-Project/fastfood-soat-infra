9e0e35f997304e412003dac1c9587eef801b20ed
Matheus de Sá <matheus.de.sa@outlook.com.br>
2025-10-03 00:24:55 -0300

    add swagger UI proxy and configuration endpoints to API Gateway

--- api.gateway/eks_integration.tf
+++ api.gateway/eks_integration.tf
@@ -41,3 +41,70 @@ resource "aws_api_gateway_integration" "swagger_integration" {
   type                    = "HTTP_PROXY"
   uri                     = "http://${var.eks-alb-dns}/api/swagger-ui/index.html"
 }
+
+resource "aws_api_gateway_resource" "swagger_ui_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  parent_id   = aws_api_gateway_resource.swagger.id
+  path_part   = "swagger-ui"
+}
+
+resource "aws_api_gateway_resource" "swagger_ui_proxy_sub" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  parent_id   = aws_api_gateway_resource.swagger_ui_proxy.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "swagger_ui_proxy_get" {
+  rest_api_id   = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id   = aws_api_gateway_resource.swagger_ui_proxy_sub.id
+  http_method   = "GET"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "swagger_ui_proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id             = aws_api_gateway_resource.swagger_ui_proxy_sub.id
+  http_method             = aws_api_gateway_method.swagger_ui_proxy_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${var.eks-alb-dns}/api/swagger-ui/{proxy}"
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+  connection_type = "VPC_LINK"
+  connection_id   = aws_api_gateway_vpc_link.eks_link.id
+}
+
+# /api-docs/swagger-config -> EKS (ALB)
+resource "aws_api_gateway_resource" "api_docs" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  parent_id   = aws_api_gateway_rest_api.fastfood_api.root_resource_id
+  path_part   = "api-docs"
+}
+
+resource "aws_api_gateway_resource" "swagger_config" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  parent_id   = aws_api_gateway_resource.api_docs.id
+  path_part   = "swagger-config"
+}
+
+resource "aws_api_gateway_method" "swagger_config_get" {
+  rest_api_id   = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id   = aws_api_gateway_resource.swagger_config.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "swagger_config_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id             = aws_api_gateway_resource.swagger_config.id
+  http_method             = aws_api_gateway_method.swagger_config_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${var.eks-alb-dns}/api/api-docs/swagger-config"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.eks_link.id
+}
