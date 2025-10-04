85f1e241f84ba6ae4970155b87df4a27b422cce3
Matheus de Sá <matheus.de.sa@outlook.com.br>
2025-10-04 03:04:44 -0300

    add proxy integration for EKS in API Gateway with authentication

--- api.gateway/eks_integration.tf
+++ api.gateway/eks_integration.tf
@@ -20,15 +20,17 @@ resource "aws_api_gateway_integration" "clients_post_integration" {
   connection_id           = aws_api_gateway_vpc_link.eks_link.id
 }
 
-# Root (ANY) -> EKS (ALB), protegido pelo authorizer
-resource "aws_api_gateway_integration" "root_any_protected_integration" {
+# /{proxy+} (ANY) -> EKS (ALB), com autenticação, mapeando para /api/{proxy} no EKS
+resource "aws_api_gateway_integration" "proxy_any_integration" {
   rest_api_id             = aws_api_gateway_rest_api.fastfood_api.id
-  resource_id             = aws_api_gateway_rest_api.fastfood_api.root_resource_id
-  http_method             = aws_api_gateway_method.root_any_protected.http_method
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_any.http_method
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${var.eks-alb-dns}/api"
+  uri                     = "http://${var.eks-alb-dns}/api/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.eks_link.id
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
 }
-
